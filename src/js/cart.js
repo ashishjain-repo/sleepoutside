af80ee270ab24d686ec32668218040821795de58
Ashish Jain <ashishjain99@yahoo.com>
2025-01-18 10:30:53 -0700

    Fixed Cart do add products

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,11 @@ import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  const stringed = JSON.stringify(cartItems);
+  const parsed = JSON.parse(stringed);
+  // const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector('.product-list').innerHTML = cartItemTemplate(parsed)
+  // document.querySelector('.product-list').innerHTML = htmlItems.join('');
 }
 
 function cartItemTemplate(item) {
