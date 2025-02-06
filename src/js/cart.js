c32ff90f5823aa687ae1cf197e45b49b1d6eefde
Ashish Jain <ashishjain99@yahoo.com>
2025-02-06 14:19:51 -0700

    Team activity 4

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,47 +1,5 @@
-// import { getLocalStorage } from './utils.mjs';
-
-// function renderCartContents() {
-//   const cartItems = getLocalStorage('so-cart');
-//   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-//   document.querySelector('.product-list').innerHTML = htmlItems.join('');
-// }
-
-// function cartItemTemplate(item) {
-//   const newItem = `<li class='cart-card divider'>
-//   <a href='#' class='cart-card__image'>
-//     <img
-//       src='${item.Image}'
-//       alt='${item.Name}'
-//     />
-//   </a>
-//   <a href='#'>
-//     <h2 class='card__name'>${item.Name}</h2>
-//   </a>
-//   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
-//   <p class='cart-card__quantity'>qty: 1</p>
-//   <p class='cart-card__price'>$${item.FinalPrice}</p>
-// </li>`;
-
-//   return newItem;
-// }
-// function cartTotal(){
-//   let total = 0;
-//   const cart =  document.getElementsByClassName('cart-total')[0];
-//   try{
-//     const cartItems = getLocalStorage('so-cart');
-//     // cartItems.reduce((item) => total += item.FinalPrice);
-//     total = cartItems.reduce((total, item) => total + item.FinalPrice, 0);
-//     cart.append(`${total}`);
-//     document.getElementsByClassName('cart-footer')[0].classList.remove('hide');
-//   }
-//   catch(error){}
-// }
-// cartTotal();
-
-// renderCartContents();
-
 import { getLocalStorage } from "./utils.mjs";
-
+import { renderHeaderFooter } from "./utils.mjs";
 // Function to render cart contents
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || []; // Ensure we handle empty cart gracefully
@@ -123,3 +81,4 @@ document.addEventListener("DOMContentLoaded", () => {
 cartTotal();
 renderCartContents();
 addItemDiscount();
+renderHeaderFooter();
